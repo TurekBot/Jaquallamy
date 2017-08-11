40b79afadb1f4fa35a987b9fd94e327f6fb4495f
Codester3310 <Cody.turek@hotmail.com>
2017-08-11 13:52:02 -0600

    Added a weapons class and implemented a weapon Array for RNG intensive purposes

--- src/com/turekbros/games/jaquallamy/Game.java
+++ src/com/turekbros/games/jaquallamy/Game.java
@@ -4,6 +4,8 @@ import com.turekbros.games.jaquallamy.levels.Level1;
 
 import java.util.Scanner;
 
+import static com.turekbros.games.jaquallamy.levels.Level1.say;
+
 public class Game {
     /**
      * This is how the user sends us input. Use methods like .next() and nextLine() to get input.
@@ -16,50 +18,61 @@ public class Game {
     private static String name;
     private static String land;
 
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println("Welcome to Jaquallamy, a Text Based Game by");
-        System.out.println
-                ("╔════╦╗─────╔════╗──────╔╗─╔══╗\n" +
-                        "║╔╗╔╗║║─────║╔╗╔╗║──────║║─║╔╗║\n" +
-                        "╚╝║║╚╣╚═╦══╗╚╝║║╠╣╔╦═╦══╣║╔╣╚╝╚╦═╦══╦══╗\n" +
-                        "──║║─║╔╗║║═╣──║║║║║║╔╣║═╣╚╝╣╔═╗║╔╣╔╗║══╣\n" +
-                        "──║║─║║║║║═╣──║║║╚╝║║║║═╣╔╗╣╚═╝║║║╚╝╠══║\n" +
-                        "──╚╝─╚╝╚╩══╝──╚╝╚══╩╝╚══╩╝╚╩═══╩╝╚══╩══╝\n");
 
-        System.out.println("What do you call yourself? ");
+    public static void main(String[] args) throws InterruptedException {
+        int i;
+        long time=50L;
+
+
+        say("\"Welcome to Jaquallamy, a Text Based Game by", time);
+
+
+        say("╔════╦╗─────╔════╗──────╔╗─╔══╗\n" +
+                "║╔╗╔╗║║─────║╔╗╔╗║──────║║─║╔╗║\n" +
+                "╚╝║║╚╣╚═╦══╗╚╝║║╠╣╔╦═╦══╣║╔╣╚╝╚╦═╦══╦══╗\n" +
+                "──║║─║╔╗║║═╣──║║║║║║╔╣║═╣╚╝╣╔═╗║╔╣╔╗║══╣\n" +
+                "──║║─║║║║║═╣──║║║╚╝║║║║═╣╔╗╣╚═╝║║║╚╝╠══║\n" +
+                "──╚╝─╚╝╚╩══╝──╚╝╚══╩╝╚══╩╝╚╩═══╩╝╚══╩══╝\n", 5L);
+
+
+        say("\"What do you call yourself? ", time);
         name = keyboard.nextLine();
 
-        System.out.println("Ay, " + name + "...");
 
-        for (int i = 0; i < 3; i++) {
-            Thread.sleep(1000);
-            System.out.print(".");
+
+        say("Ay, " + name + "...", time);
+
+            for (i = 0; i < 3; i++) {
+                Thread.sleep(1000);
+                System.out.print(".");
+            }
+
+        say("...and from where do you hail, oh, " + name + "?", time);
+            land = keyboard.nextLine();
+
+            say("*A mist swirls around you*", time);
+
+            for (i = 0; i < 3; i++) {
+                System.out.print(".");
+                Thread.sleep(1000);
+            }
+
+            say("Welcome to Jaquallamy, " + name + " of " + land, time);
+
+            // TODO: 8/10/2017 Let's add a method that automates the delay of text so that things look cleaner.
+            // TODO: 8/11/2017 Let's add
+            for (i = 0; i < 3; i++) {
+                System.out.print(".");
+                Thread.sleep(1000);
+            }
+
+       say("Press any key to begin Level 1...", time);
+
+            Scanner keyboard = new Scanner(System.in);
+            keyboard.hasNextLine();
+            Level1.start();
+
         }
-
-        System.out.println("...and from where do you hail, oh, " + name + "?");
-        land = keyboard.nextLine();
-
-        System.out.println("*A mist swirls around you*");
-
-        for (int i = 0; i < 3; i++) {
-            System.out.print(".");
-            Thread.sleep(1000);
-        }
-
-        System.out.println("Welcome to Jaquallamy, " + name + " of " + land);
-
-        // TODO: 8/10/2017 Let's add a method that automates the delay of text so that things look cleaner.
-        for (int i = 0; i < 3; i++) {
-            System.out.print(".");
-            Thread.sleep(1000);
-        }
-
-        System.out.println("Press any key to begin Level 1...");
-
-        Scanner keyboard = new Scanner(System.in);
-        keyboard.hasNextLine();
-        Level1.start();
-    }
 
 
     public static String getName() {
--- src/com/turekbros/games/jaquallamy/levels/Level1.java
+++ src/com/turekbros/games/jaquallamy/levels/Level1.java
@@ -3,11 +3,13 @@ package com.turekbros.games.jaquallamy.levels;
 import com.turekbros.games.jaquallamy.Game;
 
 import java.time.Duration;
+import java.util.Random;
 import java.util.Scanner;
+
 
 public class Level1 {
     /**
-     * This is how the user sends input.
+     * This is how the user sends input.◙
      */
 
     private static Scanner keyboard = new Scanner(System.in);
@@ -16,42 +18,88 @@ public class Level1 {
      * Try game.getName(); or game.getLand();
      */
     private static Game game;
+    private static String[] weapons = {"a Scymitar" ,"a Bow and Arrow" ,"a Katana" ,"a Soup can Lid" ,"a Dagger" ,"a Plow", "glasses", "sand", "THEEEE Rapier", "a Fish-stick", "a Portal Gun",
+            "a Broadsword"};
 
-    public static void start  ()throws InterruptedException {
+    public static void say(String s, long delay) throws InterruptedException { // this method delays text characters
+        for (int i = 0; i < s.length(); i++) {
+            System.out.print(s.charAt(i));
+            Thread.sleep(delay);
+        }
+        System.out.println(""); // space
+    }
+
+    public static void start() throws InterruptedException {
         String[] listLine = new String[500];
         listLine[0] = "\n\nLevel 1 Begin: \n\n";
         listLine[1] = "You are on a war-torn Plateau";
-        listLine[2] = "\"Upon the horizon you spy something...\"";
+        listLine[2] = "Upon the horizon you spy something...";
         listLine[3] = "What do you spy?";
-        System.out.println(listLine[0]);
-        for(int i = 0; i < listLine[0].length(); i++) { // for loop prints out the first line and delays it
-        System.out.println(listLine[1]);
-        Thread.sleep(1000L); //time is in milliseconds
-}
+        // see line 55 for listLine[4]
+        listLine[5] = "what will you do next?";
+        // see line ___ for listLine[10]
+        int i;
+        long time = 1L; // this is the time integer THIS DETERMINES HOW LONG THE  DELAY IS IN BETWEEN CHARACTERS
+        for (i = 0; i < listLine[0].length(); i++) { // for loop delays individual chars in string
+            System.out.print(listLine[0].charAt(i));
+            Thread.sleep(time); //time is in milliseconds
+        }
+        System.out.println(""); // this is the space in between lines
 
-        System.out.println(listLine[2]);
 
-        System.out.println(listLine[3]);
-        String answer = keyboard.nextLine();
+        say("You are on a war-torn Plateau", time);
 
-        listLine[4] = "Ah, you can now definitely see it is " + answer + " coming towards you.";
 
-        System.out.println("Ah, you can now definitely see it is " + answer + " coming towards you.");
-        System.out.println(Game.getName() +" what will you do next?");
+        say("Upon the horizon you spy something...", time);
+
+        say("What do you spy?", time);
+
+
+        String answer = keyboard.nextLine(); //user's reply
+
+        listLine[4] = "Ah, you can now definitely see it is " + answer + " coming towards you."; // listLine[4] needs to be here to utilize String var "answer"
+
+        say("Ah, you can now definitely see it is " + answer + " coming towards you.", time);
+
+
+        say("what will you do next?", time);
+        say("1. Run in the opposite direction", time);
+        say("2. Sneak up on the " + answer, time);
+        say("3. Stay put", time);
+        say("4. Sprint towards "+ answer + " while swinging " + getRandomWeapon(), time);
+
+
         String action = keyboard.nextLine();
-        System.out.println("You decide to " + action);
+        listLine[10] = "You decide to " + action; //listLine[6] is here to utilize String var "action"
+
+        for (i = 0; i < listLine[10].length(); i++) { //prints line 10
+            System.out.print(listLine[10].charAt(i));
+            Thread.sleep(time);
+        }
         System.out.println("");
 
 
     }
 
+    private static String getRandomWeapon() {
+        Random r = new Random();
+        int weaponNumber  = r.nextInt(12);
+
+        return weapons[weaponNumber];
+    }
+
     /**
      * A convenience method that prints out the message one key at a time in an effort to simulate someone
      * talking or typing.
+     *
      * @param message the message that will be output to the user, character by character
-     * @param delay the delay between outputting characters
+     * @param delay   the delay between outputting characters
      */
     private void say(String message, Duration delay) throws InterruptedException {
+        //Get the string as a bunch of chars
 
+        //Then for each character, print it at the specified delay.
     }
 }
+
+
