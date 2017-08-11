dfd98be66d0c9921d291669a9243506a48998a06
Bradley Turek <TurekBot@gmail.com>
2017-08-10 22:30:43 -0600

    Asking user what they spy.

--- src/com/turekbros/games/jaquallamy/levels/Level1.java
+++ src/com/turekbros/games/jaquallamy/levels/Level1.java
@@ -1,9 +1,19 @@
 package com.turekbros.games.jaquallamy.levels;
 
+import java.util.Scanner;
+
 public class Level1 {
+    private static Scanner keyboard = new Scanner(System.in);
+
+
     public static void start() {
         System.out.println("\n\nLevel 1 Begin: \n\n");
 
         System.out.println("You are on a war-torn plateau.");
+        System.out.println("Upon the horizon you spy something...");
+
+        System.out.println("What do you spy?");
+        String answer = keyboard.nextLine();
+
     }
 }
