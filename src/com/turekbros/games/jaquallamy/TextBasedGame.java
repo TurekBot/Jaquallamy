1ae8ca01137cf67cde0b637ad1ab15197649e1b9
Bradley Turek <TurekBot@gmail.com>
2017-08-10 23:01:02 -0600

    Now getting the user's name and land in the most ominous of ways.

--- src/com/turekbros/games/jaquallamy/TextBasedGame.java
+++ src/com/turekbros/games/jaquallamy/TextBasedGame.java
@@ -5,8 +5,18 @@ import com.turekbros.games.jaquallamy.levels.Level1;
 import java.util.Scanner;
 
 public class TextBasedGame {
+    /**
+     * This is how the user sends us input. Use methods like .next() and nextLine() to get input.
+     */
+    private static Scanner keyboard = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    /**
+     * The user's name
+     */
+    public static String name;
+    public static String land;
+
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Welcome to Jaquallamy, a Text Based Game by");
         System.out.println
                 ("╔════╦╗─────╔════╗──────╔╗─╔══╗\n" +
@@ -15,10 +25,48 @@ public class TextBasedGame {
                         "──║║─║╔╗║║═╣──║║║║║║╔╣║═╣╚╝╣╔═╗║╔╣╔╗║══╣\n" +
                         "──║║─║║║║║═╣──║║║╚╝║║║║═╣╔╗╣╚═╝║║║╚╝╠══║\n" +
                         "──╚╝─╚╝╚╩══╝──╚╝╚══╩╝╚══╩╝╚╩═══╩╝╚══╩══╝\n");
+
+        System.out.println("What do you call yourself? ");
+        String name = keyboard.nextLine();
+
+        System.out.println("Ay, " + name + "...");
+
+        for (int i = 0; i < 3; i++) {
+            Thread.sleep(1000);
+            System.out.print(".");
+        }
+
+        System.out.println("...and from where do you hail, oh, " + name + "?");
+        land = keyboard.nextLine();
+
+        System.out.println("*A mist swirls around you*");
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print(".");
+            Thread.sleep(1000);
+        }
+
+        System.out.println("Welcome to Jaquallamy, " + name + " of " + land);
+
+        // TODO: 8/10/2017 Let's add a method that automates the delay of text so that things look cleaner.
+        for (int i = 0; i < 3; i++) {
+            System.out.print(".");
+            Thread.sleep(1000);
+        }
+
         System.out.println("Press any key to begin Level 1...");
 
         Scanner keyboard = new Scanner(System.in);
         keyboard.hasNextLine();
         Level1.start();
+    }
+
+
+    public static String getName() {
+        return name;
+    }
+
+    public static String getLand() {
+        return land;
     }
 }
