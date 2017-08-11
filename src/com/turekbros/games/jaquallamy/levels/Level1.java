6a51923e3a6079c26e478fd136abdc69f97fef0c
Bradley Turek <TurekBot@gmail.com>
2017-08-10 23:01:47 -0600

    Added ability to get global things like the user's name and land.

--- src/com/turekbros/games/jaquallamy/levels/Level1.java
+++ src/com/turekbros/games/jaquallamy/levels/Level1.java
@@ -1,10 +1,19 @@
 package com.turekbros.games.jaquallamy.levels;
 
+import com.turekbros.games.jaquallamy.TextBasedGame;
+
 import java.util.Scanner;
 
 public class Level1 {
+    /**
+     * This is how the user sends input.
+     */
     private static Scanner keyboard = new Scanner(System.in);
-
+    /**
+     * This is to access global things like the user's name and land
+     * Try game.getName(); or game.getLand();
+     */
+    private TextBasedGame game;
 
     public static void start() {
         System.out.println("\n\nLevel 1 Begin: \n\n");
