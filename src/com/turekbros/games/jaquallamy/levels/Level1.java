aa81c9671af880c0a88832bb3e8a6a15f9977abf
Codester3310 <Cody.turek@hotmail.com>
2017-08-10 23:09:07 -0600

    Working on an Array System to make database storage of text easier to edit and allows for the implementation of String Char Delay.

--- src/com/turekbros/games/jaquallamy/levels/Level1.java
+++ src/com/turekbros/games/jaquallamy/levels/Level1.java
@@ -3,18 +3,29 @@ package com.turekbros.games.jaquallamy.levels;
 import java.util.Scanner;
 
 public class Level1 {
+
     private static Scanner keyboard = new Scanner(System.in);
 
 
-    public static void start() {
-        System.out.println("\n\nLevel 1 Begin: \n\n");
+    public static void start  ()throws InterruptedException {
+        String[] listLine = new String[500];
+        listLine[0] = "\n\nLevel 1 Begin: \n\n";
+        listLine[1] = "You are on a war-torn Plateau";
+        listLine[2] = "\"Upon the horizon you spy something...\"";
+        listLine[3] = "What do you spy?";
+        listLine[4] = "Ah, you can now definitely see it is " + answer + " coming towards you.";
+        System.out.println(listLine[0]);
 
-        System.out.println("You are on a war-torn plateau.");
-        System.out.println("Upon the horizon you spy something...");
+        System.out.println(listLine[1]);
+        System.out.println(listLine[2]);
 
-        System.out.println("What do you spy?");
+        System.out.println(listLine[3]);
         String answer = keyboard.nextLine();
-        System.out.println("Ah, you can now definitely see it is a " + answer + " coming towards you.");
+        System.out.println("Ah, you can now definitely see it is " + answer + " coming towards you.");
+        System.out.pintln(name +" what will you do next?");
+        String action = keyboard.nextLine();
+        System.out.println("You decide to " + action);
+        System.out.println("");
 
 
     }
