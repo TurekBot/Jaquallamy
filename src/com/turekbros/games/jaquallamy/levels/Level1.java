8205ac51174fae65bbe7bb26604b23258e2142da
Codester3310 <Cody.turek@hotmail.com>
2017-08-10 22:25:57 -0600

    I added code in an effort to write the prologue of Lvl 1.

--- src/com/turekbros/games/jaquallamy/levels/Level1.java
+++ src/com/turekbros/games/jaquallamy/levels/Level1.java
@@ -3,5 +3,7 @@ package com.turekbros.games.jaquallamy.levels;
 public class Level1 {
     public static void start() {
         System.out.println("\n\nLevel 1 Begin: \n\n");
+
+        System.out.println("You are on a war-torn plateau.");
     }
 }
