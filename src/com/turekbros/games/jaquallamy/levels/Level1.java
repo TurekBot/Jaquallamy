3e3e3442eb3202f5c238377abaf647e8f1d14890
Codester3310 <Cody.turek@hotmail.com>
2017-08-10 23:16:08 -0600

    Working on an Array System to make database storage of text easier to edit and allows for the implementation of String Char Delay.

--- src/com/turekbros/games/jaquallamy/levels/Level1.java
+++ src/com/turekbros/games/jaquallamy/levels/Level1.java
@@ -14,7 +14,7 @@ public class Level1 {
      * This is to access global things like the user's name and land
      * Try game.getName(); or game.getLand();
      */
-    private TextBasedGame game;
+    private static TextBasedGame game;
 
     public static void start  ()throws InterruptedException {
         String[] listLine = new String[500];
@@ -22,7 +22,7 @@ public class Level1 {
         listLine[1] = "You are on a war-torn Plateau";
         listLine[2] = "\"Upon the horizon you spy something...\"";
         listLine[3] = "What do you spy?";
-        listLine[4] = "Ah, you can now definitely see it is " + answer + " coming towards you.";
+
         System.out.println(listLine[0]);
 
         System.out.println(listLine[1]);
@@ -30,8 +30,11 @@ public class Level1 {
 
         System.out.println(listLine[3]);
         String answer = keyboard.nextLine();
+
+        listLine[4] = "Ah, you can now definitely see it is " + answer + " coming towards you.";
+
         System.out.println("Ah, you can now definitely see it is " + answer + " coming towards you.");
-        System.out.pintln(name +" what will you do next?");
+        System.out.println(game.getName() +" what will you do next?");
         String action = keyboard.nextLine();
         System.out.println("You decide to " + action);
         System.out.println("");
