5e036b75a4a197ce67de1df586572d6080cce001
Bradley Turek <TurekBot@gmail.com>
2017-08-13 15:57:05 -0600

    + RNG will always be bounded by the jokes array, no need to update it * Fixed some typos in the jokes. * Fixed some commenting

--- src/com/turekbros/games/jaquallamy/levels/Level1.java
+++ src/com/turekbros/games/jaquallamy/levels/Level1.java
@@ -27,19 +27,23 @@ public class Level1 {
     private static String[] opponent = {"Jeremy", "Mist the Dragon", "Birds", "A Lion", "a Possessed Staple Gun", "a younger version of yourself", "Dr. Impaired", "a United State Quarter"
             , "your Ex-Boyfriend", "your Ex-Girlfriend", "Lad", "a dancing flower", "a Dark Knight", "a Daemon", "a Fallen Star"};
 
-    public static String[] jokes = {"Programmer(noun.) A machine that turns coffee into code", "What do you call a programmer from Finland? ", "What do computers and air conditioners hve in common?",
-            "Why do Java programmers have to wear glasses?", "what happens to a frog's car when it breaks down?", "what did the duck say when he bought lipstick?"};
+    private static String[] jokes = {"Programmer(noun.)", "What do you call a programmer from Finland? ", "What do computers and air conditioners have in common?",
+            "Why do Java programmers have to wear glasses?", "What happens to a frog's car when it breaks down?", "What did the duck say when he bought lipstick?"};
 
-    public static String[] punchLines = {"A machine that turns coffee into code.", "Nerdic.", "They both become useless when you open Windows.", "because they don't C#.", "It gets toad away."
-            , "put it on my bill."};
-
-    private final static long time = 50L; // this is the time integer THIS DETERMINES HOW LONG THE  DELAY IS IN BETWEEN CHARACTERS
+    private static String[] punchLines = {"A machine that turns coffee into code.", "Nerdic.", "They both become useless when you open Windows.", "Because they don't C#.", "It gets toad away."
+            , "Put it on my bill."};
+    /**
+     * this is the time integer
+     * THIS DETERMINES HOW LONG THE DELAY IS IN BETWEEN CHARACTERS
+     */
+    private final static long time = 50L;
 
     public static void say(String s, long delay) throws InterruptedException { // this method delays text characters
         for (int i = 0; i < s.length(); i++) {
             System.out.print(s.charAt(i));
             Thread.sleep(delay);
         }
+        // FIXME: 8/13/2017 I think that two double quotes need a space in them to be a space—also, why are we printing a space?
         System.out.println(""); // space
     }
 
@@ -47,8 +51,6 @@ public class Level1 {
      * A convenience method that prints out the message one key at a time in an effort to simulate someone
      * talking or typing.
      */
-
-
     public static void start() throws InterruptedException {
 
         int[] action = new int[50]; //an array designated for in-game choices
@@ -61,8 +63,8 @@ public class Level1 {
         weapon[0] = getRandomWeapon();
         String[] option = new String[50];
 
-        /**
-         * This String Array will be used to record and declare their decisions
+        /*
+          This String Array will be used to record and declare their decisions
          */
 
         say("Level 1 Begin:", time);
@@ -78,15 +80,15 @@ public class Level1 {
         say(option[1] = "2. Bury the chest deeper into the ground.", time);
         say(option[2] = "3. Walk past the chest.", time);
         say(option[3] = "4. Crack a witty joke.", time);
-        /**
-         * option[0-3] branch off into their own story trees
-         * option[3] pulls from a joke database that's currently in the works
+        /*
+          option[0-3] branch off into their own story trees
+          option[3] pulls from a joke database that's currently in the works
          */
 
 
         action[0] = keyboard.nextInt();
-        /**
-         * This is how the user sends input.◙
+        /*
+          This is how the user sends input.◙
          */
         while (proceed == 0) {
             if (action[0] == 1) { //option 1 branch
@@ -95,8 +97,8 @@ public class Level1 {
                 say("You open the chest and you discover " + weapon[0] + " inside!", time);
             }
 
-            /**
-             * end of option 1's tree story branch
+            /*
+              end of option 1's tree story branch
              */
 
 
@@ -120,8 +122,8 @@ public class Level1 {
                     }
                 }
             }
-            /**
-             * end of option 2's tree story branch
+            /*
+              end of option 2's tree story branch
              */
 
 
@@ -130,8 +132,8 @@ public class Level1 {
                 say(decide + option[2], time); //option 3
             }
 
-            /**
-             * end of option 3's tree story branch
+            /*
+              end of option 3's tree story branch
              */
 
             else if (action[0] == 4) {
@@ -139,14 +141,14 @@ public class Level1 {
                 sayRandomJoke(); //option 4
             }
 
-            /**
-             * end of option 4's tree story branch
+            /*
+              end of option 4's tree story branch
              */
 
             else
                 say("Option not available. (Use options 1-4)", time);
-            /**
-             * @param while requires the user to make a choice
+            /*
+              @param while requires the user to make a choice
              * @param If statements are used to allocate the user's choices
              * @param else resets the while loop
              */
@@ -206,26 +208,21 @@ public class Level1 {
     /**
      * This method tells the player a joke. It can't return both the joke and punch line to a say method like the
      * other get methods because methods can only return one thing.
-     *
+     * <p>
      * To solve this, *this* method is in control of when the things get said, and doesn't return anything (that's
      * why it's called sayRandomJoke, not getRandomJoke.
+     *
      * @throws InterruptedException in case the say method fails.
      */
     private static void sayRandomJoke() throws InterruptedException {//this method pulls from an array of Random Jokes to humor the player with
         Random r = new Random();
-        int jokeNumber = r.nextInt(5);
+        int jokeNumber = r.nextInt(jokes.length);
 
         say(jokes[jokeNumber], time);
         say(".................................................", time);
         say(punchLines[jokeNumber], time);
     }
 }
-
-
-/**
- * @param message the message that will be output to the user, character by character
- * @param delay   the delay between outputting characters
- */
 
 
 
